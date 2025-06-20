5c69802264f3373964b07e55283ce82964dec0bb
pawan <kumarpawan9041491@gmail.com>
2025-06-20 22:13:04 +0530

    new update 2 by pawan

--- src/Components/Section7.jsx
+++ src/Components/Section7.jsx
@@ -8,7 +8,6 @@ import web6 from "../Images/clone/lakeview.png";
 import { Link } from "react-router-dom";
 // **************************************
 
-
 import { useEffect } from "react";
 import Aos from "aos";
 export default function Section7() {
@@ -19,12 +18,12 @@ export default function Section7() {
     { id: 3, img1: web3, srclink: "https://jkstucco.ca/" },
     { id: 4, img1: web4, srclink: "http://werloans.com/" },
     { id: 6, img1: web6, srclink: "https://lakeviewhomescalgary.ca/" },
-    { id: 5, img1: web5, srclink: "https://ministryofcardetailing.com.au/" }
-  ]
+    { id: 5, img1: web5, srclink: "https://ministryofcardetailing.com.au/" },
+  ];
   // ********************************
   useEffect(() => {
-    Aos.init({ duration: 1500, once: false, })
-  }, [])
+    Aos.init({ duration: 1500, once: false });
+  }, []);
   return (
     <>
       <section>
@@ -35,61 +34,52 @@ export default function Section7() {
               <div className="col-md-12  mt-5">
                 {/* *************** H@ */}
                 <h2 className="font-styleX text-center " data-aos="fade-up">
-                  L<strong className="bgcolortext">i</strong>ve P<strong className="bgcolortext">r</strong>oj
+                  L<strong className="bgcolortext">i</strong>ve P
+                  <strong className="bgcolortext">r</strong>oj
                   <strong className="bgcolortext">ec</strong>t
                 </h2>
                 {/* ************** Light */}
                 <div className="light mt-3">
-                  <div className=" text-center light-line">
-                  </div>
+                  <div className=" text-center light-line"></div>
                 </div>
               </div>
             </div>
             {/* ********************* */}
             <div className="row  mt-5 pt-5" data-aos="zoom-in-up">
-              {
-                project.map((item) => {
-                  return (
-                    <>
-                      <div className="col-md-6 col-lg-4 mt-4"  >
-                        {/* ************************* Live peojects */}
-                        <div className="project-card ">
-
-
-                          <CardContainer className="inter-var" key={item?.id}>
-                            <CardBody
-                              className=" relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black   w-auto sm:w-[30rem] h-auto rounded-xl p-2   ">
-                              {/* <CardItem
+              {project.map((item) => {
+                return (
+                  <>
+                    <div className="col-md-6 col-lg-4 mt-4">
+                      {/* ************************* Live peojects */}
+                      <div className="project-card ">
+                        <CardContainer className="inter-var" key={item?.id}>
+                          <CardBody className=" relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]   w-auto sm:w-[30rem] h-auto rounded-xl p-2   ">
+                            {/* <CardItem
           translateZ="50"
           className=" font-bold text-neutral-950 text-align justify-center text-center">
          Ministry of Car Detailing
         </CardItem> */}
 
-                              <CardItem translateZ="100" className="w-full ">
-                                <Link to={item.srclink}>
-                                  <img
-                                    src={item?.img1}
-
-                                    className="w-full object-cover  group-hover/card:shadow-xl"
-                                    alt="thumbnail" />
-                                </Link>
-                              </CardItem>
-
-
-                            </CardBody>
-                          </CardContainer>
-
-                        </div>
+                            <CardItem translateZ="100" className="w-full ">
+                              <Link to={item.srclink}>
+                                <img
+                                  src={item?.img1}
+                                  className="w-full object-cover  group-hover/card:shadow-xl"
+                                  alt="thumbnail"
+                                />
+                              </Link>
+                            </CardItem>
+                          </CardBody>
+                        </CardContainer>
                       </div>
-                    </>
-                  )
-                })
-              }
+                    </div>
+                  </>
+                );
+              })}
             </div>
-
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
